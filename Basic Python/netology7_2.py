1de9732666b263df070664b5256b4490597f88cf
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-08-24 07:30:59 +0700

    Modified code for working the same elements in list

--- Basic Python/netology7_2.py
+++ Basic Python/netology7_2.py
@@ -11,17 +11,30 @@ def get_shop_list_by_dishes(dishes, person_count):
     list_of_last_quantity = []
     list_measure = []
 
-    for product_of_dish in dishes:
-        our_list = cook_book[product_of_dish]
+    if len(dishes) == len(set(dishes)):
+        for product_of_dish in dishes:
+            print(product_of_dish)
+            our_list = cook_book[product_of_dish]
+            for key in our_list:
+                list_of_ingredients.append(key['ingredient_name'])
+                list_of_quantity.append(key['quantity'])
+                list_measure.append(key['measure'])
+            for amount_of_person in list_of_quantity:
+                list_of_last_quantity.append(int(amount_of_person) * person_count)
+        for ingredients, measure, quantity in zip(list_of_ingredients, list_measure, list_of_last_quantity):
+            dict_of_necessary_ingredients = dict.fromkeys([ingredients], dict(measure=measure, quantity=quantity))
+            print(dict_of_necessary_ingredients)
+    else:
+        our_list = cook_book[dishes[0]]
         for key in our_list:
             list_of_ingredients.append(key['ingredient_name'])
             list_of_quantity.append(key['quantity'])
             list_measure.append(key['measure'])
         for amount_of_person in list_of_quantity:
-            list_of_last_quantity.append(int(amount_of_person) * person_count)
-    for ingredients, measure, quantity in zip(list_of_ingredients, list_measure, list_of_last_quantity):
-        dict_of_necessary_ingredients = dict.fromkeys([ingredients], dict(measure=measure, quantity=quantity))
-        print(dict_of_necessary_ingredients)
+            list_of_last_quantity.append(int(amount_of_person) * person_count * len(dishes))
+        for ingredients, measure, quantity in zip(list_of_ingredients, list_measure, list_of_last_quantity):
+            dict_of_necessary_ingredients = dict.fromkeys([ingredients], dict(measure=measure, quantity=quantity))
+            print(dict_of_necessary_ingredients)
 
 
-get_shop_list_by_dishes(['Омлет', 'Запеченный картофель'], 2)
+get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)
