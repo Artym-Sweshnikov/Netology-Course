b4108e4c27e22943026646528e36a3620daafa63
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-08-30 10:31:36 +0700

    Added newsafr.json for netology8_1.py

---
+++ Basic Python/netology9_2.py
@@ -0,0 +1,4 @@
+import requests
+
+response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload?path=Netology/Authorization: AgAAAAAVRsa2AAaSFgTvyhB_bkF-pF3w0JcD13A')
+print(response)
