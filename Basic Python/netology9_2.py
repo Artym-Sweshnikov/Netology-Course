dbd1db0952a96625066689c6ef85dfc28dfa88c6
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-09-03 19:29:15 +0700

    Wroten code for netology9_2

--- Basic Python/netology9_2.py
+++ Basic Python/netology9_2.py
@@ -1,4 +1,24 @@
 import requests
+import file as fl
 
-response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload?path=Netology/Authorization: AgAAAAAVRsa2AAaSFgTvyhB_bkF-pF3w0JcD13A')
-print(response)
+keys_dict = fl.function()
+token = keys_dict['API_KEY']
+
+
+class YaUploader:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def upload(self):
+        """Метод загруджает файлы по списку file_list на яндекс диск"""
+        headers = {'Authorization': token}
+        response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                                headers=headers, params={'path': self.file_path})
+        response_json = response.json()
+        requests.put(response_json['href'])
+        print('Success!')
+
+
+if __name__ == '__main__':
+    uploader = YaUploader('/Folder/file153.txt')
+    result = uploader.upload()
