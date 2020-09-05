b1206b6eea1c886799fc225df6b5c9e26a6d273d
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-09-05 20:00:58 +0700

    Coding is over for this time

--- Basic Python/netology9_1.py
+++ Basic Python/netology9_1.py
@@ -1,8 +1,12 @@
-from pprint import pprint
 import requests
 
-response = requests.get('https://superheroapi.com/api/2619421814940190/search/Hulk')
-pprint(response)
-dict_results = response.json()['results']
-pprint(dict_results)
-print(5)
+name_list = ['Hulk', 'Captain_America', 'Thanos']
+traits_list = []
+
+for hero in name_list:
+    response = requests.get('https://superheroapi.com/api/2619421814940190/search/' + hero)
+    dict_results = response.json()['results']
+    for value in dict_results:
+        traits_list.append([value['name'], value['powerstats']['intelligence']])
+
+print(max(traits_list, key=lambda x: int(x[1])))
