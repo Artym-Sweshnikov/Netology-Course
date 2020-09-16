0a6a85c2516fd17c3d91a2e59b217264282db87f
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-09-16 13:09:32 +0700

    Test

--- Basic Python/test.py
+++ Basic Python/test.py
@@ -1,41 +1,150 @@
-cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
+import requests
+import json
+import file as fl
+from pprint import pprint
+from datetime import datetime
+from tqdm import tqdm
+from time import sleep
+
+keys_dict = fl.function()
+TOKEN_YANDEX = keys_dict['APP_YANDEX']
+TOKEN_VK = keys_dict['APP_VK']
 
 
-def get_shop_list_by_dishes(dishes, person_count):
-    '''Функция состовляет список продуктов, необходимых для приготовления блюда.
+def create_folder(name_folder):
+    '''Создает новую директорию на Яндекс.Диск.
 
-    Пользователь выбирает блюда и передает количество человек в аргумент функции.'''
-    new_dict = {}
-    for dish in dishes:
-        if dish in cook_book:
-            recipe = cook_book[dish]
-            for dict_of_recipe in recipe:
-                amount_of_product = (dict_of_recipe['quantity'] * person_count)
-                dict_of_dish = dict(dish=dish, dict_of_recipe=dict_of_recipe)
-                new_dict = dict.fromkeys([dish], dict(measure=dict_of_recipe['measure'], quantity=dict_of_recipe['quantity']))
-                if dish in dict_of_dish['dish']:
-                    new_dict[dish]['quantity'] = amount_of_product
-                    print(new_dict)
-                else:
-                    new_dict[dish]['quantity'] += dict_of_recipe['quantity']
-                    print(new_dict)
+    Папке присваевается имя id пользователя в вк.'''
+    headers = {'Authorization': TOKEN_YANDEX}
+    response_ya_disk = requests.put(
+        'https://cloud-api.yandex.net/v1/disk/resources',
+        headers=headers,
+        params={
+            'path': name_folder
+        })
 
 
-get_shop_list_by_dishes(['Омлет','Запеченный картофель'], 3)
+def upload(name, URL):
+    '''Загружает полученные фотографии на Диск.
+
+    Принимает только URL путь файла.'''
+    print(name, URL)
+    headers = {'Authorization': TOKEN_YANDEX}
+    response_ya_disk = requests.post(
+        'https://cloud-api.yandex.net/v1/disk/resources/upload',
+        headers=headers,
+        params={
+            'url': URL,
+            'path': f'/{folder_name(id_vk_user)}/' + name
+        })
+
+
+def get_largest(size_dict):
+    '''Находит картинук максимального размера.
+
+    Для сравнения подаются итерируемые объекты.'''
+    if size_dict['width'] >= size_dict['height']:
+        return size_dict['width']
+    else:
+        return size_dict['height']
+
+
+def search_id(user_input):
+    '''Ищем id пользователя в вк.
+
+    Проверяем правильность пользовательский ввода.'''
+    if user_input.isdigit() is True and len(user_input) == 9:
+        return user_input
+    else:
+        response_searcher = requests.get(
+            'https://api.vk.com/method/users.get',
+            params={
+                'access_token': TOKEN_VK,
+                'user_ids': user_input,
+                'v': 5.122
+            }
+        )
+        for i in response_searcher.json()['response']:
+            return i['id']
+
+
+def folder_name(user_input):
+    '''Называет директорию.
+
+    Присваеват директории имя пользователя.'''
+    response_searcher = requests.get(
+        'https://api.vk.com/method/users.get',
+        params={
+            'access_token': TOKEN_VK,
+            'user_ids': user_input,
+            'lang': 0,
+            'v': 5.122
+        }
+    )
+    for i in response_searcher.json()['response']:
+        return i['last_name']
+
+
+def photo_name(element):
+    for el in element:
+        image = str(el['likes']['count']) + '.jpg'
+        if image in name_list:
+            ts = int(el['date'])
+            name_list.append(image[:-4] + datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d%H:%M:%S') + '.jpg')
+        else:
+            name_list.append(image)  # Добавляем имя фотографии в формате jpg
+
+
+def json_file():
+    '''Создает json файл.
+
+    Файл сохраняется в директории запущенной программы.'''
+    result_list = []
+    for result_name, result_size in zip(name_list[0:5], type_list[0:5]):
+        result = {'file_name': result_name, 'size': result_size}
+        result_list.append(result)
+    with open('photos.json', 'w') as file:
+        json.dump(result_list, file, indent=2, ensure_ascii=False)
+
+
+url_list = []
+name_list = []
+type_list = []
+
+id_vk_user = input('Введите ID или Username пользователя вк: ')
+
+response_vk = requests.get(
+    'https://api.vk.com/method/photos.get',
+    params={
+        'access_token': TOKEN_VK,
+        'owner_id': search_id(id_vk_user),
+        'album_id': 'profile',
+        'rev': 1,
+        'extended': 1,
+        'photo_sizes': 1,
+        'v': 5.122
+    }
+)
+
+# Преобразуем полученные данные с API VK в списки
+data = response_vk.json()
+photo_list = data['response']['items']
+photo_name(photo_list)
+for photo in photo_list:
+    sizes = photo['sizes']
+    max_size = max(sizes, key=get_largest)
+    url_list.append(max_size['url'])  # Добавляем URL фотографий
+    type_photo = max_size['type']
+    type_list.append(type_photo)  # Добавляем тип фотографий
+
+# Объеденяем полученные списки с именем фотографий и их URL в один словарь не более 5 элементов
+photo_name_dict = dict(zip(name_list[0:5], url_list[0:5]))
+# Создаём новую директорию на Яндекс.Диск
+create_folder(folder_name(id_vk_user))
+# Внедряем прогресс бар для отслеживания результата загрузки фотографий
+for key, value in tqdm(photo_name_dict.items(), desc='Download Photos',
+                       unit='photo', dynamic_ncols=True, leave=True):
+    upload(key, value)
+    sleep(.1)
+json_file()
+print('https://disk.yandex.ru/client/disk')
