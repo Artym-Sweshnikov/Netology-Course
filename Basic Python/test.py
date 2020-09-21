79fe9b521e35bec3d2c51146c2426ca382214416
artym_sweshnikov <ar.sweshnikov@yandex.ru>
2020-09-21 21:27:31 +0700

    Object-oriented programming code was rewrote

--- Basic Python/test.py
+++ Basic Python/test.py
@@ -11,140 +11,144 @@ TOKEN_YANDEX = keys_dict['APP_YANDEX']
 TOKEN_VK = keys_dict['APP_VK']
 
 
-def create_folder(name_folder):
-    '''Создает новую директорию на Яндекс.Диск.
+class Document:
+    """Класс Документы."""
 
-    Папке присваевается имя id пользователя в вк.'''
-    headers = {'Authorization': TOKEN_YANDEX}
-    response_ya_disk = requests.put(
-        'https://cloud-api.yandex.net/v1/disk/resources',
-        headers=headers,
-        params={
-            'path': name_folder
-        })
+    def identifier(self):
+        """Метод для проверки пользовательского ввода."""
+        id_vk_user = input('Введите ID или Username пользователя вк: ')
+        if id_vk_user.isdigit() is True and len(id_vk_user) == 9:
+            return id_vk_user
+        else:
+            response_searcher = requests.get(
+                'https://api.vk.com/method/users.get',
+                params={
+                    'access_token': TOKEN_VK,
+                    'user_ids': id_vk_user,
+                    'v': 5.122
+                }
+            )
+            response_list = response_searcher.json()
+            for response in response_list:
+                if 'error' in response:
+                    print('Вы ввели неверное имя пользователя')
+                    quit()
+                else:
+                    id_vk_user = response['response']['id']
+                    return id_vk_user
 
+    def interrogate(self, identification):
+        """Метод запрашивает данные c сервера и возвращает список фотографий."""
 
-def upload(name, URL):
-    '''Загружает полученные фотографии на Диск.
+        def largest(size_dict):
+            """Функция находит картинук максимального размера."""
+            if size_dict['width'] >= size_dict['height']:
+                return size_dict['width']
+            else:
+                return size_dict['height']
 
-    Принимает только URL путь файла.'''
-    print(name, URL)
-    headers = {'Authorization': TOKEN_YANDEX}
-    response_ya_disk = requests.post(
-        'https://cloud-api.yandex.net/v1/disk/resources/upload',
-        headers=headers,
-        params={
-            'url': URL,
-            'path': f'/{folder_name(id_vk_user)}/' + name
-        })
+        response_vk = requests.get(
+            'https://api.vk.com/method/photos.get',
+            params={
+                'access_token': TOKEN_VK,
+                'owner_id': identification,
+                'album_id': 'profile',
+                'rev': 1,
+                'extended': 1,
+                'photo_sizes': 1,
+                'v': 5.122
+            }
+        )
 
+        response_list = []
+        database = response_vk.json()
+        photo_list = database['response']['items']
+        for photos in photo_list:
+            date = photos['date']
+            sizes = photos['sizes']
+            max_size = max(sizes, key=largest)
+            response_dict = {'date': date, 'like': photos['likes']['count'], 'photo': dict(max_size)}
+            response_list.append(response_dict)
+        return response_list[:5]
 
-def get_largest(size_dict):
-    '''Находит картинук максимального размера.
-
-    Для сравнения подаются итерируемые объекты.'''
-    if size_dict['width'] >= size_dict['height']:
-        return size_dict['width']
-    else:
-        return size_dict['height']
-
-
-def search_id(user_input):
-    '''Ищем id пользователя в вк.
-
-    Проверяем правильность пользовательский ввода.'''
-    if user_input.isdigit() is True and len(user_input) == 9:
-        return user_input
-    else:
+    def folder(self, identification):
+        """Метод для создания директории на Яндекс.Диск"""
         response_searcher = requests.get(
             'https://api.vk.com/method/users.get',
             params={
                 'access_token': TOKEN_VK,
-                'user_ids': user_input,
+                'user_ids': identification,
+                'lang': 0,
                 'v': 5.122
             }
         )
-        for i in response_searcher.json()['response']:
-            return i['id']
+        for response in response_searcher.json()['response']:
+            last_name = response['last_name']
+            headers = {'Authorization': TOKEN_YANDEX}
+            response_ya_disk = requests.put(
+                'https://cloud-api.yandex.net/v1/disk/resources',
+                headers=headers,
+                params={
+                    'path': last_name
+                })
+            return last_name
+
+    def designate(self, dictionary):
+        """Метод возвращает словарь с новыми именами"""
+        name = []
+        dates = []
+        name_dict = []
+        for picture in dictionary:
+            likes = str(picture['like'])
+            date_time = picture['date']
+            date = datetime.fromtimestamp(date_time)
+            date = date.strftime('%d.%m.%Y-%H:%M:%S')
+            name.append(likes)
+            dates.append(date)
+        length = len(name)
+        for i in range(length - 1):
+            for j in range(i + 1, length):
+                if name[i] == name[j]:
+                    name[i] += dates[i]
+                    name[j] += dates[j]
+        for index, image in enumerate(dictionary):
+            image['photo']['name'] = [score
+                                      for thing, score in enumerate(name) if thing == index]
+            indicator = image['photo']['name'][0]
+            image['photo']['name'] = indicator
+        return dictionary
+
+    def upload(self, new_dictionary, path):
+        """Метод загружает фотографии на диск"""
+        for picture in tqdm(new_dictionary, desc='Download Photos', unit='photo', dynamic_ncols=True, leave=False):
+            name = str(picture['photo']['name']) + '.jpg'
+            url = picture['photo']['url']
+            headers = {'Authorization': TOKEN_YANDEX}
+            response_ya_disk = requests.post(
+                'https://cloud-api.yandex.net/v1/disk/resources/upload',
+                headers=headers,
+                params={
+                    'url': url,
+                    'path': f'/{path}/' + name
+                })
+            sleep(.1)
+        print('Файлы успешно загруженны на Яндекс.Диск\n>>>', 'https://disk.yandex.ru/client/disk')
+
+    def json_file(self, result_dictionary):
+        """Метод создает json"""
+        result_list = []
+        for image in result_dictionary:
+            result = {'file_name': image['photo']['name'], 'size': image['photo']['type']}
+            result_list.append(result)
+        with open('result.json', 'w') as file:
+            json.dump(result_list, file, indent=2, ensure_ascii=False)
 
 
-def folder_name(user_input):
-    '''Называет директорию.
-
-    Присваеват директории имя пользователя.'''
-    response_searcher = requests.get(
-        'https://api.vk.com/method/users.get',
-        params={
-            'access_token': TOKEN_VK,
-            'user_ids': user_input,
-            'lang': 0,
-            'v': 5.122
-        }
-    )
-    for i in response_searcher.json()['response']:
-        return i['last_name']
-
-
-def photo_name(element):
-    for el in element:
-        image = str(el['likes']['count']) + '.jpg'
-        if image in name_list:
-            ts = int(el['date'])
-            name_list.append(image[:-4] + datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d%H:%M:%S') + '.jpg')
-        else:
-            name_list.append(image)  # Добавляем имя фотографии в формате jpg
-
-
-def json_file():
-    '''Создает json файл.
-
-    Файл сохраняется в директории запущенной программы.'''
-    result_list = []
-    for result_name, result_size in zip(name_list[0:5], type_list[0:5]):
-        result = {'file_name': result_name, 'size': result_size}
-        result_list.append(result)
-    with open('photos.json', 'w') as file:
-        json.dump(result_list, file, indent=2, ensure_ascii=False)
-
-
-url_list = []
-name_list = []
-type_list = []
-
-id_vk_user = input('Введите ID или Username пользователя вк: ')
-
-response_vk = requests.get(
-    'https://api.vk.com/method/photos.get',
-    params={
-        'access_token': TOKEN_VK,
-        'owner_id': search_id(id_vk_user),
-        'album_id': 'profile',
-        'rev': 1,
-        'extended': 1,
-        'photo_sizes': 1,
-        'v': 5.122
-    }
-)
-
-# Преобразуем полученные данные с API VK в списки
-data = response_vk.json()
-photo_list = data['response']['items']
-photo_name(photo_list)
-for photo in photo_list:
-    sizes = photo['sizes']
-    max_size = max(sizes, key=get_largest)
-    url_list.append(max_size['url'])  # Добавляем URL фотографий
-    type_photo = max_size['type']
-    type_list.append(type_photo)  # Добавляем тип фотографий
-
-# Объеденяем полученные списки с именем фотографий и их URL в один словарь не более 5 элементов
-photo_name_dict = dict(zip(name_list[0:5], url_list[0:5]))
-# Создаём новую директорию на Яндекс.Диск
-create_folder(folder_name(id_vk_user))
-# Внедряем прогресс бар для отслеживания результата загрузки фотографий
-for key, value in tqdm(photo_name_dict.items(), desc='Download Photos',
-                       unit='photo', dynamic_ncols=True, leave=True):
-    upload(key, value)
-    sleep(.1)
-json_file()
-print('https://disk.yandex.ru/client/disk')
+if __name__ == '__main__':
+    photo = Document()
+    ID = photo.identifier()
+    data = photo.interrogate(ID)
+    disk_path = photo.folder(ID)
+    new_names_dict = photo.designate(data)
+    photo.upload(new_names_dict, disk_path)
+    photo.json_file(new_names_dict)
